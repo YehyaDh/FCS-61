d555878c974f752afea13695988c6e7b25aca305
Yehya Dhaini <yehya.dh99@gmail.com>
2025-02-21 16:34:22 +0200

    Exercise 3

--- Homework1.py
+++ Homework1.py
@@ -20,5 +20,11 @@ if num % 2 == 0:
 else:
     print("The number is odd.")
 
+# Exercise 3
+username = input("Enter username: ")
+password = input("Enter password: ")
 
-
+if username == "admin" and password == "1234":
+    print("Access granted")
+else:
+    print("Access denied")
